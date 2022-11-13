afef805bad8f1b29bfc59985c47f5d6a74c91192
SayFu11a <sgishlakaev@mail.ru>
2022-11-13 12:35:09 +0300

    create components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from './components/Card';
+import Header from './components/Header';
+import Drawer from './components/Drawer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return <div className="wrapper clear">
+
+    <Drawer />
+    <Header />
+
+    <div className="content p-40">
+      <div className="d-flex align-center justify-between mb-40">
+        <h1>Все кроссовки</h1>
+        <div className="serch-block d-flex">
+          <img src="\img\serch.svg" alt="Serch" />
+          <input type="text" placeholder="Поиск..." />
+        </div>
+      </div>
+
+      {/* <div className="row">
+        <div className="column">
+          <div className="cart">
+            <div className="cart-img">
+              <div className="cart-favorite">
+                <img src="img\favorite.png" alt="" />
+              </div>
+              <div className="cart-png"><img src="/img/sneakers/01.jpg" alt="" /></div>
+            </div>
+            <div className="cart-title">
+              <p>Мужские Кроссовки</p>
+              <p>Nike Blazer Mid Suede</p>
+            </div>
+            <div className="cart-footer">
+              <div className="cart-price">
+                <p>Цена:</p>
+                <p>12 999 руб.</p>
+              </div>
+              <div className="cart-cross">
+                <img src="\img\arr.svg" alt="" />
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div> */}
+
+      <div className="d-flex">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+
+      </div>
+
     </div>
-  );
+  </div>
+
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+function Header() {
+   return (
+      <header className="d-flex justify-between align-center p-40">
+         <div className="align-center d-flex">
+            <img width={40} height={40} src="/img/h001.png" alt="" />
+            <div>
+               <h3>REACT SNEAKERS</h3>
+               <p className="opacity-5">Магазин лучших кроссовок</p>
+            </div>
+         </div>
+
+         <ul className="d-flex">
+            <li className="mr-30">
+               <img width={18} height={17} src="/img/h02.svg" alt="" />
+               <span>1205 руб.</span>
+            </li>
+            <li>
+               <img width={21} height={19} src="/img/h03.svg" alt="" />
+            </li>
+            <li>
+               <img className="img-mr0" width={20} height={20} src="/img/h04.svg" alt="" />
+            </li>
+         </ul>
+      </header>
+   );
+}
+
+export default Header;
